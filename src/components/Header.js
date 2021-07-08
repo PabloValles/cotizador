5601eb57e7e84a4efd619e22d569b838d5c259a9
PabloValles <valles.pablo@hotmail.com>
2021-07-08 12:14:07 -0300

    Creando app de cotizador de seguros

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled'
+
+
+const ContenedorHeader = styled.header`
+    background-color: #26c6da;
+    padding: 15px;
+    font-weight: bold;color: #fff;
+`
+const TextoHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`
+
+const Header = ({titulo}) => {
+
+
+
+    return ( 
+        <ContenedorHeader>
+            <TextoHeader> {titulo} </TextoHeader>
+        </ContenedorHeader>
+     );
+}
+ 
+export default Header;
